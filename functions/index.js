840579dbd325ffc6c517733d66005cf2d1cf3fa8
Nick Bruhnev <nicholaspeterb@gmail.com>
2020-11-05 17:45:39 +0200

    add fcm

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ const db = admin.firestore();
 
 exports.notifyUsersOfMoviesInCommon = functions.region('europe-west1').firestore
     .document('users/{userId}/accepted/{movieId}')
-    .onCreate( (doc, ctx) => {
+    .onCreate((doc, ctx) => {
 
         const { imdbID } = doc.data();
 
@@ -22,55 +22,97 @@ exports.notifyUsersOfMoviesInCommon = functions.region('europe-west1').firestore
         const friendsRef = currUserDoc.collection('friends');
 
         let currUserName = "";
+        let currUserToken = "";
         db.collection('users').doc(currUserDoc.id).get().then(currUser => {
             currUserName = currUser.data().name;
+            currUserToken = currUser.data().fcmToken;
             return null;
         }).catch((error) => {
             console.log("Error getting currUser document:", error);
         });
-        
 
-        friendsRef.get().then( (querySnapshot) => {
+
+        friendsRef.get().then((querySnapshot) => {
 
             //Loop through the friends collection
-            querySnapshot.forEach( (friend) => {
+            querySnapshot.forEach((friend) => {
 
                 let friendUid = friend.data().uid;
                 let friendName = friend.data().name;
+                let friendToken = friend.data().token;
                 let isMovieInCommon = db.collection('users').doc(friendUid).collection('accepted').doc(imdbID);
 
-                isMovieInCommon.get().then( (movie) => {
+                isMovieInCommon.get().then((movie) => {
                     if (movie.exists) {
 
-                        // Notify friend
+                        // -- Notify Friend --
+
+                        //Add notification to db 
                         db.collection('users').doc(friendUid).collection('notifications').add({
                             type: 'just-liked',
                             msg: currUserName + ' just liked ' + movie.data().Title + '!',
                             friendUid: currUserDoc.id,
                             movieInCommon: imdbID,
                             createdAt: admin.firestore.FieldValue.serverTimestamp(),
-                        }).catch( (error) => {
+                        }).catch((error) => {
                             console.error("Error adding notification document: ", error);
                         });
 
-                        // Notify current user
+                        if (friendToken) {
+                            // Send friend a message
+                            var messageFriend = {
+                                data: {
+                                    msg: friendName + ' just liked ' + movie.data().Title + '!',
+                                },
+                                token: friendToken
+                            };
+                            admin.messaging().send(messageFriend)
+                                .then((response) => {
+                                    console.log('Successfully sent message:', response);
+                                    return null;
+                                })
+                                .catch((error) => {
+                                    console.log('Error sending message:', error);
+                                });
+                        }
+
+                        // -- Notify Current User --
+
+                        //Add notification to db 
                         db.collection('users').doc(currUserDoc.id).collection('notifications').add({
                             type: 'also-likes',
                             msg: friendName + ' also likes ' + movie.data().Title + '!',
                             friendUid,
                             movieInCommon: imdbID,
                             createdAt: admin.firestore.FieldValue.serverTimestamp(),
-                        }).catch( (error) => {
+                        }).catch((error) => {
                             console.error("Error adding notification document: ", error);
                         });
 
+                        if (currUserToken) {
+                            // Send current user a message
+                            var messageUser = {
+                                data: {
+                                    msg: friendName + ' also likes ' + movie.data().Title + '!',
+                                },
+                                token: currUserToken
+                            };
+                            admin.messaging().send(messageUser)
+                                .then((response) => {
+                                    console.log('Successfully sent message:', response);
+                                    return null;
+                                })
+                                .catch((error) => {
+                                    console.log('Error sending message:', error);
+                                });
+                        }
                     }
-                    return null;  
+                    return null;
                 }).catch((error) => {
                     console.log("Error getting isMovieInCommon document:", error);
                 });
             });
-            return null;  
+            return null;
         }).catch((error) => {
             console.log("Error getting friendsRef document:", error);
         });
