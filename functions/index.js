e9a16872e27abd68b233d336494f88f9c303ac31
Nick Bruhnev <nicholaspeterb@gmail.com>
2020-11-06 00:57:53 +0200

    rewrote function to avoid nested promises

--- functions/index.js
+++ functions/index.js
@@ -1,132 +1,50 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
 
 exports.notifyUsersOfMoviesInCommon = functions.region('europe-west1').firestore
     .document('users/{userId}/accepted/{movieId}')
-    .onCreate((doc, ctx) => {
+    .onCreate( async (doc, context) => {
 
         const { imdbID } = doc.data();
 
-        const currUserDoc = doc.ref.parent.parent;
-        const friendsRef = currUserDoc.collection('friends');
+        const userId = context.params.userId;
+        const userRef = db.collection('users').doc(userId);
 
-        let currUserName = "";
-        //let currUserToken ;
-        db.collection('users').doc(currUserDoc.id).get().then(currUser => {
-            currUserName = currUser.data().name;
-            //currUserToken = currUser.data().fcmToken;
-            return null;
-        }).catch((error) => {
-            console.log("Error getting currUser document:", error);
-        });
+        const userData = (await userRef.get()).data();
+        const userName = userData.name;
 
+        const friendsRef = userRef.collection('friends');
+        const friendsSnapshot = await friendsRef.get();
 
-        friendsRef.get().then((querySnapshot) => {
+        //Loop through the friends collection
+        friendsSnapshot.forEach( async (friend) => {
+            let friendUid = friend.data().uid;
+            let friendName = friend.data().name;
 
-            //Loop through the friends collection
-            querySnapshot.forEach((friend) => {
+            let movieRef = db.collection('users').doc(friendUid).collection('accepted').doc(imdbID);
+            let movie = await movieRef.get();
 
-                let friendUid = friend.data().uid;
-                let friendName = friend.data().name;
-                
-                /*let friendToken ;
-                db.collection('users').doc(friendUid).get().then(fri => {
-                    friendToken = fri.data().fcmToken;
-                    return null;
-                }).catch((error) => {
-                    console.log("Error getting friend's token:", error);
-                });*/
-
-                let isMovieInCommon = db.collection('users').doc(friendUid).collection('accepted').doc(imdbID);
-
-                isMovieInCommon.get().then((movie) => {
-                    if (movie.exists) {
-
-                        // -- Notify Friend --
-
-                        //Add notification to db 
-                        db.collection('users').doc(friendUid).collection('notifications').add({
-                            type: 'just-liked',
-                            msg: currUserName + ' just liked ' + movie.data().Title + '!',
-                            friendUid: currUserDoc.id,
-                            movieInCommon: imdbID,
-                            createdAt: admin.firestore.FieldValue.serverTimestamp(),
-                        }).catch((error) => {
-                            console.error("Error adding notification document: ", error);
-                        });
-
-                        /*if (friendToken) {
-                            // Send friend a message
-                            var messageFriend = {
-                                data: {
-                                    msg: currUserName + ' just liked ' + movie.data().Title + '!',
-                                },
-                                token: friendToken
-                            };
-                            admin.messaging().send(messageFriend)
-                                .then((response) => {
-                                    console.log('Successfully sent message:', response);
-                                    return null;
-                                })
-                                .catch((error) => {
-                                    console.log('Error sending message:', error);
-                                });
-                        } else {
-                            console.log('currUser: ' + currUserName + ' - ' + 'friend ' + friendName +' has no token: ' + friendToken);
-                        }*/
-
-                        // -- Notify Current User --
-
-                        //Add notification to db 
-                        db.collection('users').doc(currUserDoc.id).collection('notifications').add({
-                            type: 'also-likes',
-                            msg: friendName + ' also likes ' + movie.data().Title + '!',
-                            friendUid,
-                            movieInCommon: imdbID,
-                            createdAt: admin.firestore.FieldValue.serverTimestamp(),
-                        }).catch((error) => {
-                            console.error("Error adding notification document: ", error);
-                        });
-
-                        /*if (currUserToken) {
-                            // Send current user a message
-                            var messageUser = {
-                                data: {
-                                    msg: friendName + ' also likes ' + movie.data().Title + '!',
-                                },
-                                token: currUserToken
-                            };
-                            admin.messaging().send(messageUser)
-                                .then((response) => {
-                                    console.log('Successfully sent message:', response);
-                                    return null;
-                                })
-                                .catch((error) => {
-                                    console.log('Error sending message:', error);
-                                });
-                        } else {
-                            console.log('currUser has no token');
-                            console.log('friendName', friendName);
-                        }*/
-                    }
-                    return null;
-                }).catch((error) => {
-                    console.log("Error getting isMovieInCommon document:", error);
+            if (movie.exists) {
+                // -- Notify Friend --
+                await db.collection('users').doc(friendUid).collection('notifications').add({
+                    type: 'just-liked',
+                    msg: userName + ' just liked ' + movie.data().Title + '!',
+                    friendUid: userId,
+                    movieInCommon: imdbID,
+                    createdAt: admin.firestore.FieldValue.serverTimestamp(),
                 });
-            });
-            return null;
-        }).catch((error) => {
-            console.log("Error getting friendsRef document:", error);
+
+                // -- Notify Current User --
+                await db.collection('users').doc(userId).collection('notifications').add({
+                    type: 'also-likes',
+                    msg: friendName + ' also likes ' + movie.data().Title + '!',
+                    friendUid,
+                    movieInCommon: imdbID,
+                    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+                });
+            }
         });
     });
